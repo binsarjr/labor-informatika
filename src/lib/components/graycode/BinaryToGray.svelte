5091d6910859dd7807caaf9b938ee27878ff1373
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-06-28 11:33:54 +0700

    chore: recode some line

--- src/lib/components/graycode/BinaryToGray.svelte
+++ src/lib/components/graycode/BinaryToGray.svelte
@@ -18,7 +18,6 @@
 		type="text"
 		bind:value
 		on:input={onChange}
-		pattern="[0-1]*"
 		class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 	/>
 	<p>Result: {result}</p>
